c244fb56a906ad8a9ca4b332cc6c29f9ee60be8c
irene <cozy92@gmail.com>
2019-11-12 21:29:09 +1100

    Web Application - Use Django

--- Irene.py
+++ Irene.py
@@ -1,10 +1,12 @@
 # import requests
 import json
+import common
 
 class ioT:
     def __init__(self, name = "ioT Device"):
         f = open("TodayWeather.json", "r")
         todayWeather = f.read()
+        #todayWeather = common.decode(todayWeather)
         self.weather = json.loads(todayWeather)
      #   self.weather['Sunset'] = datetime.fromtimestamp(self.weather['Sunset'])
         f.close()
