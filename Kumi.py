e11c15ece59282b1ad638d9edaed0fa342a37394
Kumi Lieu <kumi.lieu@gmail.com>
2019-11-15 16:39:52 +1100

    Tested 15/11/19

--- Kumi.py
+++ Kumi.py
@@ -1,4 +1,4 @@
-# provide menu for (1) adding items and print ADD (2) deleting items and print DELETE (3) print list SHOW (4) assistance HELP commands
+# provide menu for (1) adding items and print ADD (2) deleting items and print DELETE (3) print list SHOW (4) assistance HELP (5) exit QUIT commands
 
 # provide list of all items
 
@@ -15,16 +15,15 @@ def show_help():
   1. ADD : to add new item and print shopping list
   2. DELETE : to delete item and print shopping list
   3. SHOW : to print list of current items
-  4. HELP : to bring up menu  
-  Q. Quit : 
+  4. HELP : to bring up menu    
+  5. QUIT : to exit app
 
   -----------------------------------------------------------
   """)
 
     user = input("What is your name?\n")
     print("Hello " + user + "!")
-    print(
-        "Enter item you need and press enter. Repeat for every new item to add. When finished, make a selection from the above menu")
+    print("Type each item you need and press enter. When done, make a selection from the above menu from 1 to 5.")
 
 def show_list():
     # print current list of items
@@ -35,7 +34,7 @@ def show_list():
         print(item)
 
 def add_item_to_list(new_item):
-    # add new item to shopping list
+    # add new item to list
 
     shopping_list.append(new_item)
     print("{} has been added. Your Shopping List now has {} items.".format(new_item, len(shopping_list)))
@@ -49,6 +48,7 @@ while True:
     # user have option to quit app
     if new_item == "1":
         show_list()
+        print("Let’s hit the road! You are ready to go shopping!")
         break
 
     # user will be asked for item to delete
@@ -56,16 +56,18 @@ while True:
         delete_item = input("Which item would you like to remove?\n")
         shopping_list.remove(delete_item)
         show_list()
-        continue
+        print("Let’s hit the road! You are ready to go shopping!")
+        break
     elif new_item == "3":
         show_list()
-        continue
+        print("Let’s hit the road! You are ready to go shopping!")
+        break
     elif new_item == "4":
         show_help()
         continue
-    elif new_item == "Q":
+    elif new_item == "5":
         show_list()
-        print("Now, You can go to the grocery !!!")
+        print("Let’s hit the road! You are ready to go shopping!")
 
         with open("ShoppingList.json", "w") as fout:
            fout.write(str(shopping_list))
